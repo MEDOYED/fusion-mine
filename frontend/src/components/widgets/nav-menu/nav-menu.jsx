5cc85ecbf24f097dcd7799fd4245d1bd2eea9961
MEDOYED <mmax26072005@gmail.com>
2024-12-08 23:31:01 +0100

    frontend | v2 nav menu

--- frontend/src/components/widgets/nav-menu/nav-menu.jsx
+++ frontend/src/components/widgets/nav-menu/nav-menu.jsx
@@ -1,6 +1,8 @@
 import "./nav-menu.css";
 
-import navLogo from "../../../assets/fusion-mine-logo.png";
+import navLogo from "../../../assets/NavMenu/fusion-mine-logo.png";
+import discordIcon from "../../../assets/NavMenu/discord-icon.svg";
+import vkIcon from "../../../assets/NavMenu/vk.svg";
 
 const NavMenu = () => {
   return (
@@ -14,8 +16,16 @@ const NavMenu = () => {
           <li className="nav__link">О проекте</li>
         </ul>
         <ul className="nav__sm-links">
-          <li className="nav__sm-link">D</li>
-          <li className="nav__sm-link">VK</li>
+          <li className="nav__sm-link">
+            <a href="https://discord.gg/3xHFJ95Y">
+              <img src={discordIcon} alt="Ссилка на дискорд" />
+            </a>
+          </li>
+          <li className="nav__sm-link">
+            <a href="#">
+              <img src={vkIcon} alt="Ссилка на Вконтакте" />
+            </a>
+          </li>
         </ul>
       </nav>
     </>
